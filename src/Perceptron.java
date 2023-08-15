31ac58cfaade677141162648a2491616142e1f8b
navarhontes <sophiawan01@gmail.com>
2023-08-15 18:44:02 -0400

    javadocs for train method, made train method stop if no more changes

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -9,14 +9,10 @@ public class Perceptron {
     public Perceptron(int inputSize) {
         this.inputSize = inputSize; 
 
-        // Currently always initializas bias and weights randomly. 
         weights = new double[inputSize];
-        //Random rand = new Random();
         for (int i = 0; i < inputSize; i++) {
             weights[i] = 0; 
-            //weights[i] = rand.nextDouble();
         }
-        //bias = rand.nextDouble();
         bias = 0;
     }
 
@@ -38,24 +34,40 @@ public class Perceptron {
         for (int i = 0; i < inputs.length; i++) {
             weightedSum += inputs[i] * weights[i];
         }
-        // don't know what's going on here 
-        double output = weightedSum + bias > 0 ? 1 : 0;
+        // if (weightedSum + bias > 0), then set to 1; else 0
+        double output = (weightedSum + bias > 0) ? 1 : 0;
         return (int) output;
     }
     
+    /**
+     * @param inputs array of length l (arbitrary) containing arrays of size inputSize that represent the inputs to be categorised 
+     * @param target array of length l representing the desired outputs for those labels 
+     * @param epochs number of times to run the perceptron algorithm 
+     */
     public void train(double[][] inputs, int[] target, int epochs) {
-        for (int epoch = 0; epoch < epochs; epoch++) {
-            // should probably make it stop if no updates 
+        // weightsUpdated tracks whether any updates are actually happening or not, 
+        // so that the training can stop if the categorizations are all correct 
+        // and therefore not being updated. 
+        boolean weightsUpdated = true; 
+
+        int epoch = 0; 
+        while (epoch < epochs && weightsUpdated){
+            weightsUpdated = false; 
+            
             for (int i = 0; i < inputs.length; i++) {
                 int prediction = predict(inputs[i]);
                 int error = target[i] - prediction;
 
                 for (int j = 0; j < weights.length; j++) {
-                    // is 0 1 nicer or + - nicer? 
                     weights[j] += error * inputs[i][j];
                 }
                 bias += error;
+
+                if(error != 0){
+                    weightsUpdated = true; 
+                }
             }
+            epoch++; 
         }
     }
 }
