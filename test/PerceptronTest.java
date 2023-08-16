fe1f847b360a776afd90e095abae83efe166acef
navarhontes <sophiawan01@gmail.com>
2023-08-15 21:13:58 -0400

    Added more standard test cases

--- test/PerceptronTest.java
+++ test/PerceptronTest.java
@@ -29,6 +29,21 @@ public class PerceptronTest {
     }
 
     @Test
+    public void testANDGate3DimentionalInput() {
+        double[][] inputs = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1}, {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}};
+        int[] target = {0, 0, 0, 0, 0, 0, 0, 1};
+
+        Perceptron perceptron = new Perceptron(3);
+        perceptron.train(inputs, target, 100);
+        
+        int[] expectedPredictions = {0, 0, 0, 0, 0, 0, 0, 1};
+        for (int i = 0; i < inputs.length; i++) {
+            int prediction = perceptron.predict(inputs[i]);
+            assertEquals(expectedPredictions[i], prediction);
+        }
+    }
+
+    @Test
     public void testORGate() {
         double[][] inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
         int[] target = {0, 0, 0, 1};
@@ -37,6 +52,42 @@ public class PerceptronTest {
         perceptron.train(inputs, target, 100);
 
         int[] expectedPredictions = {0, 0, 0, 1};
+        for (int i = 0; i < inputs.length; i++) {
+            int prediction = perceptron.predict(inputs[i]);
+            assertEquals(expectedPredictions[i], prediction);
+        }
+    }
+
+    @Test
+    public void testXORGate() {
+        // Since this case is not linearly separable 
+        double[][] inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        int[] target = {0, 1, 1, 0};
+
+        Perceptron perceptron = new Perceptron(2);
+        perceptron.train(inputs, target, 100);
+
+        int[] expectedPredictions = {0, 0, 0, 1};
+        int[] actualPredictions = new int[4]; 
+        for (int i = 0; i < inputs.length; i++) {
+            actualPredictions[i] = perceptron.predict(inputs[i]);
+        }
+        assertNotEquals(expectedPredictions, actualPredictions); 
+    }
+
+    @Test
+    public void testMIT6036Week2Homework3() {
+        // Since this case is not linearly separable 
+        double[][] inputs = {{-3, 2}, {-1, 1}, {-1, -1}, {2, 2}, {1, -1}};
+        int[] target = {1, 0, 0, 0, 0};
+
+        Perceptron perceptron = new Perceptron(2);
+        perceptron.train(inputs, target, 100);
+
+        double[] weights = perceptron.getWeights();
+        double bias = perceptron.getBias();
+
+        int[] expectedPredictions = {1, 0, 0, 0, 0};
         for (int i = 0; i < inputs.length; i++) {
             int prediction = perceptron.predict(inputs[i]);
             assertEquals(expectedPredictions[i], prediction);
