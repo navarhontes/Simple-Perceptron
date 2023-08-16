d5733d53d078828cc1a52b535e573352c3dada55
navarhontes <sophiawan01@gmail.com>
2023-08-15 22:30:08 -0400

    First draft of TextInterface

--- src/TextInterface.java
+++ src/TextInterface.java
@@ -20,55 +20,43 @@ public class TextInterface {
             System.out.println("3. Exit");
             System.out.print("Enter your choice: ");
 
-            try {
-                int choice = scanner.nextInt();
-                scanner.nextLine(); // Consume newline
-                
-                switch (choice) {
-                    case 0:
-                        perceptronFacade = createPerceptronFacade(scanner);
-                        break;
-                    case 1:
-                        trainPerceptron(scanner, perceptronFacade);
-                        break;
-                    case 2:
-                        makePrediction(scanner, perceptronFacade);
-                        break;
-                    case 3:
-                        System.out.println("Exiting the program. Goodbye!");
-                        scanner.close();
-                        return;
-                    default:
-                        System.out.println("Invalid choice. Please choose again.");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please enter a valid integer.");
-                scanner.nextLine(); // Consume invalid input
+            int choice = getValidIntInput(scanner);
+            
+            switch (choice) {
+                case 0:
+                    perceptronFacade = createPerceptronFacade(scanner);
+                    break;
+                case 1:
+                    trainPerceptron(scanner, perceptronFacade);
+                    break;
+                case 2:
+                    makePrediction(scanner, perceptronFacade);
+                    break;
+                case 3:
+                    System.out.println("Exiting the program. Goodbye!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please choose again.");
             }
         }
     }
 
     private static PerceptronFacade createPerceptronFacade(Scanner scanner) {
         // Create and return an instance of PerceptronFacade with necessary parameters
-        while (true){
-            try {
-                int inputSize = scanner.nextInt();
-                scanner.nextLine(); // Consume newline
-                return new PerceptronFacade(inputSize); 
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please enter a valid integer.");
-                scanner.nextLine(); // Consume invalid input
-            }
-        }
+        int inputSize = getValidIntInput(scanner); 
+        return new PerceptronFacade(inputSize); 
     }
 
     private static void trainPerceptron(Scanner scanner, PerceptronFacade perceptronFacade) {
-        System.out.print("Enter the number of training data points: ");
-        int numDataPoints = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
+        System.out.print("Enter the number of training data points (must be a positive integer!): ");
+        int numDataPoints = getValidIntInput(scanner);
+        while(numDataPoints <= 0){
+            numDataPoints = getValidIntInput(scanner);
+        }
         
         int inputDimension = perceptronFacade.getInputSize();
-        int[][] inputs = new int[numDataPoints][inputDimension];
+        double[][] inputs = new double[numDataPoints][inputDimension];
         int[] targets = new int[numDataPoints];
         
         System.out.println("Enter training data in the format [input1 input2 ... inputN target]:");
@@ -76,43 +64,73 @@ public class TextInterface {
             System.out.print("Data " + (i + 1) + ": ");
             String[] data = scanner.nextLine().split(" ");
             
-            if (data.length != inputDimension + 1) {
-                System.out.println("Invalid input. Please enter " + (inputDimension + 1) + " values.");
+            if (data.length != inputDimension) {
+                System.out.println("Invalid input. Please enter " + (inputDimension) + " values.");
                 i--; // Retry this data point
                 continue;
             }
             
-            for (int j = 0; j < inputDimension; j++) {
-                inputs[i][j] = Integer.parseInt(data[j]);
+            try{
+                for (int j = 0; j < inputDimension; j++) {
+                    inputs[i][j] = Double.parseDouble(data[j]);
+                }
+            } catch(InputMismatchException e){
+                System.out.println("Invalid input. Please enter a valid decimal.");
             }
             targets[i] = Integer.parseInt(data[inputDimension]);
         }
         
-        System.out.print("Enter learning rate: ");
-        double learningRate = scanner.nextDouble();
-        scanner.nextLine(); // Consume newline
+        System.out.print("Enter number of epochs (must be a positive integer!): ");
+        int epochs = getValidIntInput(scanner);
+        while(epochs <= 0){
+            epochs = getValidIntInput(scanner);
+        }
         
-        System.out.print("Enter number of epochs: ");
-        int epochs = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
-        
-        perceptronFacade.train(inputs, targets, learningRate, epochs);
+        perceptronFacade.train(inputs, targets, epochs);
         
         System.out.println("Training completed.");
     }
 
     private static void makePrediction(Scanner scanner, PerceptronFacade perceptronFacade) {
         int inputDimension = perceptronFacade.getInputSize();
-        int[] inputs = new int[inputDimension];
+        double[] inputs = new double[inputDimension];
         
         System.out.println("Enter input values for prediction:");
         for (int i = 0; i < inputDimension; i++) {
             System.out.print("Input " + (i + 1) + ": ");
-            inputs[i] = scanner.nextInt();
+            inputs[i] = getValidDoubleInput(scanner); 
         }
         
         int prediction = perceptronFacade.predict(inputs);
         System.out.println("Prediction: " + prediction);
+    }
+
+    public static int getValidIntInput(Scanner scanner){
+        int input; 
+        while (true){
+                try {
+                    input = scanner.nextInt();
+                    scanner.nextLine(); // Consume newline
+                    return input; 
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                    scanner.nextLine(); // Consume invalid input
+                }
+            }
+    }
+
+    public static double getValidDoubleInput(Scanner scanner){
+        double input; 
+        while (true){
+            try {
+                input = scanner.nextDouble();
+                scanner.nextLine(); // Consume newline
+                return input; 
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                scanner.nextLine(); // Consume invalid input
+            }
+        }
     }
     
 }
